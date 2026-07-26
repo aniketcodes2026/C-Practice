dc3ddf9950ce674b30e7a37fae0a6e3df882e169
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-07-26 19:49:15 +0530

    Create builtin_datatypes.cpp

---
+++ Basics/builtin_datatypes.cpp
@@ -0,0 +1,17 @@
+#include<iostream> 
+using namespace std; 
+
+int main(){
+    int a = 2; 
+    int b = 7;
+    float pi = 3.14; 
+    char c = 'A';
+    bool x = true;
+    // or int a = 2,b = 7;
+    
+    cout<<"This is a tutorial. Here the value of a is "<< a <<". Here the value of b is "<< b<<".\n"; 
+    cout<<"The value of pi is " << pi<< ".\nThe value of c is "<< c <<".\nThe bool value is "<< x;
+    
+    return 0; 
+    
+}
