6bc138cb40617402990236431ab55fb3a6b68bde
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-07-30 20:23:15 +0530

    Create Operator Precedence.cpp

---
+++ Basics/Operator Precedence.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Operator Precedence
+    
+    int a =3, b=4;
+    int c = ((((a*5)+b)-45)+87); //follows bodmas rule to an extent...add and sub from left to right
+    
+    cout<<c;
+    
+    return 0;
+}
