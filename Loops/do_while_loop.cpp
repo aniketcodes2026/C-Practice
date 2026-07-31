13c09aede4dcb73c3201d09b529b56c379fc6d6d
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-07-31 20:49:08 +0530

    Create do_while_loop.cpp

---
+++ Loops/do_while_loop.cpp
@@ -0,0 +1,20 @@
+// syntax of do while loop
+  // do{
+  //     C++ statements;
+  // }while(condition);
+
+
+// Print all natural number till 20
+
+  #include<iostream>
+using namespace std;
+
+int main(){
+    int i=1;
+    do{
+        cout<<i<<endl;
+        i++;
+    }while(i<=20);
+
+    return 0;
+}
