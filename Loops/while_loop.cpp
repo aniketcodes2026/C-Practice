544e278440ce44703046dfb252018fd671953c50
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-07-31 20:42:50 +0530

    Create while_loop.cpp

---
+++ Loops/while_loop.cpp
@@ -0,0 +1,21 @@
+// syntax of while loops
+// while(condition)
+// {
+//     C++ statements;
+// }
+
+
+//Print all natural numbers till 40
+    
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int i=1;
+    while(i<=40)
+    {
+        cout<<i<<endl;
+        i++;
+    }
+}
