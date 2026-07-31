ae1ba1d5d91b28c785da3ddf03a71c5c84927d20
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-07-31 20:40:14 +0530

    Create for_loop.cpp

---
+++ Loops/for_loop.cpp
@@ -0,0 +1,19 @@
+// syntax of for loop
+  // for(initilization; condition; updation)
+  //  {
+  //      loop body(C++ code);
+  //  }  
+  
+  
+//Print all natural number till 40
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    for (int i = 1; i<=40; i++)
+    {
+        cout<<i<<endl;
+    }
+}
