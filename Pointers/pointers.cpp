5a7ea522c31fe77055345c66df8642152deecbf8
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-08-01 19:58:38 +0530

    Create pointers.cpp

---
+++ Pointers/pointers.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=3;
+    // (data type)* denotes a pointer,which holds the address of other data types
+    
+    int* b = &a;
+    // & denotes (address of) operator
+    cout<<"The address of a is "<<&a<<endl;
+    
+    cout<<"The address of a is "<<b<<endl;
+    // * denotes(value at) dereference operator
+    cout<<"The value at address b is "<<*b<<endl;
+    
+    // pointer to pointer
+    int** c = &b;
+    cout<<"The address of b is "<<&b<<endl;
+    cout<<"The address of b is "<<c<<endl;
+
+    return 0;
+    
+}
+
