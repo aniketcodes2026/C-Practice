7213df2f9d1e2ac24701968f974ef098a3b4105c
ANIKET RAJ <aniketvmc2008@gmail.com>
2026-08-03 19:54:05 +0530

    Update function prototype.cpp

--- Basics/function prototype.cpp
+++ Basics/function prototype.cpp
@@ -5,6 +5,7 @@ using namespace std;
 // type function-name(arguments);
 
 int sum(int a, int b);
+void g(void);
 
 int main(){
     int num1, num2;
@@ -21,4 +22,10 @@ int sum(int a, int b){
     // formal parameters a and b will be taking values from actual parameters num1 and num 2
     int c = a+b;
     return c;
+}
+
+void g(){
+    cout<<"\nHello,GOOD MORNING";
+
+    return 0;
 }
