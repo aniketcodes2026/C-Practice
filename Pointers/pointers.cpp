fcf6a466cc3b6f4a93acc7da0afec4377626a377
ANIKET RAJ <aniketvmc2008@gmail.com>
2026-08-04 20:32:28 +0530

    Update pointers.cpp

--- Pointers/pointers.cpp
+++ Pointers/pointers.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 
 int main(){
+   
     int a=3;
     // (data type)* denotes a pointer,which holds the address of other data types
     
     int* b = &a;
+   
     // & denotes (address of) operator
     cout<<"The address of a is "<<&a<<endl;
     
@@ -14,6 +16,7 @@ int main(){
     cout<<"The value at address b is "<<*b<<endl;
     
     // pointer to pointer
+    
     int** c = &b;
     cout<<"The address of b is "<<&b<<endl;
     cout<<"The address of b is "<<c<<endl;
