5b0c24de429708dcf90904c88ef365beb4a749de
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-08-07 20:21:12 +0530

    Create Static data member.cpp

---
+++ OOPS/Static data member.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+class Employee{
+    int Id;
+    static int  count;
+    // static gives the different count to every employee entered
+    // by default they are private
+    public:
+        void setData(void){
+            cout<<"enter the Id"<<endl;
+            cin>>Id;
+            count++;
+        }
+        void getData(void){
+            cout<<"the Id of this employee is "<<Id<<" and this is employee number "<<count<<endl;
+            
+        }
+
+};
+// count is the static data member of class employee
+int Employee :: count;
+// default value is 0
+int main(){
+    Employee suhani;
+    suhani.setData();
+    suhani.getData();
+    
+    Employee aniket;
+    aniket.setData();
+    aniket.getData();
+    return 0;
+}
