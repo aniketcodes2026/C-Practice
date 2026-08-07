a0f3ed3fc2186010976c3b28d8ab7703ecbbedab
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-08-07 20:22:58 +0530

    Create array of objects.cpp

---
+++ OOPS/array of objects.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+class Employee{
+    int id;
+    int salary;
+    public:
+        void setId(void){
+            salary = 122;
+            cout<<"enter the id of employee ";
+            cin>>id;
+        }
+        void getId(void){
+            cout<<"the id of this employee is "<<id<<endl;
+        }
+};
+
+int main(){
+    Employee fb[4];
+    for (int i = 0; i<4; i++)
+    {
+        fb[i].setId();
+        fb[i].getId();
+        
+    }
+    
+    
+}
