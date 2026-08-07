d0c8d8acbbc9f9c683c77be79a16f4fdebbcf2de
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-08-07 20:09:41 +0530

    Create Classes.cpp

---
+++ OOPS/Classes.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+class Employee
+{
+    
+    private:
+    int a, b, c;
+    public:
+    int d, e;
+    void setData(int a1, int b1, int c1);
+    void getData(){
+        cout<<"the value of a "<<a<<endl;
+        cout<<"the value of b "<<b<<endl;
+        cout<<"the value of c "<<c<<endl;
+        cout<<"the value of d "<<d<<endl;
+        cout<<"the value of e "<<e<<endl;
+    }
+};
+void Employee :: setData(int a1, int b1, int c1){
+    a = a1;
+    b = b1;
+    c = c1;
+}
+
+int main(){
+    Employee aniket;
+    aniket.d = 30;
+    aniket.e = 40;
+    // aniket.a = 20; 
+    // can't access it as variable a is private
+    aniket.setData(1,2,4);
+    aniket.getData();
+    return 0;
+}
